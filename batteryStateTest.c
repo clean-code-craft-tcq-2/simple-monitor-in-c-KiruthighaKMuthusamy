a3475118925996ade5d696b65f34928deb8eb0ed
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-08 21:10:09 +0530

    Update batteryStateTest.c

--- batteryStateTest.c
+++ batteryStateTest.c
@@ -11,16 +11,12 @@ void resultCheck(float *testTempList, float *testSOCList,float *testChargeRateLi
 	{
 		int result = batteryIsOk(testTempList[i],testSOCList[i],testChargeRateList[i],limitArray);
 			     //   assert(result == resultBattStatus[i]);
-		 printf("\n NALLAPAARU \n %d %d",result , resultBattStatus[i]);
-				for (int j=0;j<3;j++)
+		         	for (int j=0;j<3;j++)
 				{
 				assert(strncmp(batteryCond.parameter[j] , testResultarray[i].parameter[j], 7)== 0);
 				assert(strncmp(batteryCond.status[j] , testResultarray[i].status[j], 2)== 0);
-				/*assert(batteryCond.breachedValue[j]  == testResultarray[i].breachedValue[j]);*/
-		
-					printf("\n NALLAPAARU \n %s,%s",batteryCond.parameter[j] , testResultarray[i].parameter[j] );
-					       printf("\n NALLAPAARU \n %s,%s",batteryCond.status[j] , testResultarray[i].status[j]);
-					 printf("\n NALLAPAARU \n %f,%f", batteryCond.breachedValue[j]  , testResultarray[i].breachedValue[j]);
+				//assert(batteryCond.breachedValue[j]  == testResultarray[i].breachedValue[j]);		
+				printf("\n NALLAPAARU ------------------------------------\n %f,%f", batteryCond.breachedValue[j]  , testResultarray[i].breachedValue[j]);
 					
 				}
 		
@@ -38,10 +34,7 @@ int main()
 	float testTempList[]  =      {20.5 , -1.5 , 50 , 40};         
 	float testSOCList[] =        {19.5 , 40   , 80.5 , 40};
 	float testChargeRateList[] = {0.9,   0.5,   -1 , 0.3};
-	
-
- 
- 		  batteryCondition testResultArray[4] =  { {{"Temperature","StateCharge","ChaargeRate"},{"OK","LOW","HIGH"},{0,-0.5,0.1}},
+	batteryCondition testResultArray[4] =  { {{"Temperature","StateCharge","ChaargeRate"},{"OK","LOW","HIGH"},{0,-0.5,0.1}},
 		{{"Temperature","StateCharge","ChaargeRate"},{"LOW","OK","OK"},{-1.5,0,0}}
 		,{{"Temperature","StateCharge","ChaargeRate"},{"HIGH","HIGH","LOW"},{5,0.5,-1}}
 		, {{"Temperature","StateCharge","ChaargeRate"},{"OK","OK","OK"},{0,0,0}}};
