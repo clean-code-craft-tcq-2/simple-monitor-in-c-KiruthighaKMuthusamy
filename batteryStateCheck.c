c9c1250abd2fc55cec8a76112235185b01f2734b
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-08 15:41:21 +0530

    Update batteryStateCheck.c

--- batteryStateCheck.c
+++ batteryStateCheck.c
@@ -1,71 +1,42 @@
 #include "batteryStateCheck.h"
 
-breachAndValue checkLowerLimit(float input, float lowLimit)       // Avioding Duplication
+// Assert statement is encapsulated in a function 
+
+void resultCheck(float *testTempList, float *testSOCList,float *testChargeRateList, batteryCondition *testResultArray,float *limitArray, bool *resultBattStatus)
 {
-	breachAndValue result= {0,0.0};
-	if (input  < lowLimit)
-	{
-	result.status = LOW;
-	result.breachedValue = lowLimit - input ;	 // Calculate the breached value
-	}
-    return result;
+
+for (int i=0,i<3,i++)
+{
+	    bool result = batteryIsOk(testTempList[i],testSOCList[i],testChargeRateList[i],limitArray);
+		
+		for (int j=0, j<2,j++)
+		{
+		asset(strcmp(batteryCond.parameter[j] , testResultarray[i].parameter[j]));
+		}
+	    asset(floorf(batteryCond.parameter[3] * 100 / 100 == testResultarray[i].parameter[j]);
+		asset(result == resultBattStatus[i]);
 }
-
-breachAndValue checkHigherLimit(float input, float highLimit)        // Avioding Duplication
-{
-	breachAndValue result= {0,0.0};
-	if (input  > highLimit)
-	{
-	result.status = HIGH;
-	result.breachedValue = input - highLimit ;	 // Calculate the breached value
-	}
-	return result;
-}
-
-void printStatus(breachAndValue lowerResult , breachAndValue higherResult , int parameter)
-{
-	printf(" Lower Limit of %s is %s\n Higher Limit of %s is %s\n and the breached value is %0.2f",batteryCond.parameter[parameter],batteryStatus[lowerResult.status],batteryCond.parameter[parameter],batteryStatus[higherResult.status],(higherResult.breachedValue + lowerResult.breachedValue));
-}
-
-void limitCheck(float value, float *limit, int parameter)
-{
-
-breachAndValue lowerResult = checkLowerLimit(value , limit[0]);
-breachAndValue higherResult = checkHigherLimit(value , limit[1]);
-
-strcpy(batteryCond.parameter[parameter],batteryParameters[parameter]);
-strcpy(batteryCond.status[parameter],batteryStatus[lowerResult.status + higherResult.status]);  // Update the structure with HIGH\LOW status and  breached measure.
-batteryCond.breachedValue[parameter] = higherResult.breachedValue + lowerResult.breachedValue;
-
-printStatus(lowerResult,higherResult,parameter);
 
 }
 
-void temperatureIsOk(float temperature, float *tempLimitArray)
+int main() 
 {
-(limitCheck(temperature,tempLimitArray, 0));
-  
-}
-
-void SOCIsOk(float soc, float *SOCLimitArray)
-{
+    const float limitArray[] =   {0,45,20,80,0,0.8}; // Lower and Upper  Boundaries for all the 3 parametes 
+	float testTempList[]  =      {20.5 , -1.5 , 50 , 40};         
+	float testSOCList[] =        {19.5 , 40   , 80.5 , 40};
+	float testChargeRateList[] = {0.9,   0.5,   -1 , 0.3};
+	
+ /*//batteryCondition testResultArray[] = {{"Temparature","OK",0},{"SOC","Low",0.5},{"ChargeRate","HIGH",0.1}},
+ {{"Temparature","LOW",-1.5},{"SOC","OK",0},{"ChargeRate","OK",0}},
+ {{"Temparature","HIGH",5},{"SOC","HIGH",0.5},{"ChargeRate","LOW",-1}},
+ {{"Temparature","OK",0},{"SOC","OK",0},{"ChargeRate","OK",0}};*/
  
-(limitCheck(soc, SOCLimitArray, 1));
-  
-}
-
-void chargeRateIsOk(float chargeRate, float *chargeRateLimitArray)
-{  
-(limitCheck(chargeRate, chargeRateLimitArray, 2)); 
-}
-
-
-
-bool batteryIsOk(float temperature, float soc, float chargeRate, float *limitArray) 
-{
-  temperatureIsOk(temperature,&limitArray[0]);
-  SOCIsOk(soc,&limitArray[2]);
-  chargeRateIsOk(chargeRate,&limitArray[4]);
-  return((batteryCond.breachedValue[0]) || (batteryCond.breachedValue[1]) || (batteryCond.breachedValue[2]));
+    batteryCondition testResultArray[] = {{"Temparature","SOC","ChargeRate"},{"OK","Low","HIGH"},{-1.5,0,0.1}},
+	{{"Temparature","SOC","ChargeRate"},{"Low","OK","OK"},{5,0,0}},
+	{{"Temparature","SOC","ChargeRate"},{"HIGH","HIGH","Low"},{5,0.5,-1}},
+	{{"Temparature","SOC","ChargeRate"},{"OK","OK","OK"},{0,0,0}};
+	int resultBattStatus[] = {1,1,1,0};
+	
+    resultCheck(testTempList,testSOCList,testChargeRateList,testResultArray,limitArray,resultBattStatus);
 }
 
