c36bde7a5920dc3ee1af6666866bcabaa2168931
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-08 21:02:51 +0530

    Update batteryStateTest.c

--- batteryStateTest.c
+++ batteryStateTest.c
@@ -14,8 +14,8 @@ void resultCheck(float *testTempList, float *testSOCList,float *testChargeRateLi
 		 printf("\n NALLAPAARU \n %d %d",result , resultBattStatus[i]);
 				for (int j=0;j<3;j++)
 				{
-				assert(strncmp(batteryCond.parameter[j] , testResultarray[i].parameter[j])== 0);
-				assert(strncmp(batteryCond.status[j] , testResultarray[i].status[j])== 0);
+				assert(strncmp(batteryCond.parameter[j] , testResultarray[i].parameter[j], 7)== 0);
+				assert(strncmp(batteryCond.status[j] , testResultarray[i].status[j], 2)== 0);
 				assert(batteryCond.breachedValue[j]  == testResultarray[i].breachedValue[j]);*/
 		
 					printf("\n NALLAPAARU \n %s,%s",batteryCond.parameter[j] , testResultarray[i].parameter[j] );
