10b3b7ad960ed0fa0db550019271148280c1e7d0
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-08 19:29:57 +0530

    Update batteryStateCheck.c

--- batteryStateCheck.c
+++ batteryStateCheck.c
@@ -42,7 +42,7 @@ breachAndValue lowerResult = checkLowerLimit(value , limit[0]);
 breachAndValue higherResult = checkHigherLimit(value , limit[1]);
 
 /*strcpy(*(batteryCond.parameter[parameter]), batteryParameters[parameter]);*/
-	strcpy(batteryCond.status[parameter], "LOW");
+/*	strcpy(batteryCond.status[parameter], "LOW");
 /*	
 strcpy(batteryCond.status[parameter], batteryStatus[lowerResult.status + higherResult.status]);  */// Update the structure with HIGH\LOW status and  breached measure.
 batteryCond.breachedValue[parameter] = higherResult.breachedValue + lowerResult.breachedValue;
