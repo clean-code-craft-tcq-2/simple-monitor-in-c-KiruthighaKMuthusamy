8f8136e693d78d6287257547e9abca72fafe2129
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-14 11:43:02 +0530

    Update batteryStateTest.c

--- batteryStateTest.c
+++ batteryStateTest.c
@@ -5,9 +5,9 @@ int main()
 {
 	
     float limitArray[] =   {0,45,20,80,0,0.8}; // Lower and Upper  Boundaries for all the 3 parametes   
-    assert  (batteryIsOk(20,40,0.5); == FALSE);
-    assert  (batteryIsOk(50,90,0.9); == TRUE);
-    assert  (batteryIsOk(-1,10,0.1); == TRUE);
+    assert  (batteryIsOk(20,40,0.5,limitArray); == FALSE);
+    assert  (batteryIsOk(50,90,0.9,limitArray); == TRUE);
+    assert  (batteryIsOk(-1,10,0.1,limitArray); == TRUE);
 	
 	
 }
