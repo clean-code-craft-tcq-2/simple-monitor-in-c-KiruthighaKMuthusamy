7163481f4cb13afca306b48dd39f0f852d6b5a7b
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-02-15 16:36:05 +0530

    Update batteryStateCheck.c

--- batteryStateCheck.c
+++ batteryStateCheck.c
@@ -1,41 +1,57 @@
 #include "batteryStateCheck.h"
 
 batteryCondition batteryCond;
-const char* batteryParameters[] = {"Temperature", "StateofCharge", "ChargeRate"};
-const char* batteryStatus[] = {"OK", "HIGH", "LOW"};
 
-int checkLowerLimit(float input, float lowLimit)       
+DiffLanguage alertNotes[2]= {{{"Temperature", "StateofCharge", "ChargeRate"},{"OK","LOW_WARNING","LOW_ERROR","HIGH_WARNING","HIGH_ERROR"}},{{"Temperatur","Ladezustand", "Ladestrom"},{"OK","NIEDRIG_WARNUNG","NIEDRIG_ERROR","HOCH_WARNUNG","HOCH_ERROR"}}};
+
+int checkLowerLimit(float input, float lowLimit, float highLimit)       
 {   int result = OK;
 	if (input  < lowLimit)
 	{
-	result = LOW;
+	result = LOWERROR;
 	}
+	else if(input < (WARNINGPERCENTAGE *  highLimit)+lowLimit)
+	{
+	  result = LOWWARNING;
+	}
+
+	
     return result;
 }
 
 int checkHigherLimit(float input, float highLimit)      
-{
+{  
+    float highWarningLimit = highLimit - WARNINGPERCENTAGE * highLimit;
+    
 	int result = OK;
 	if (input  > highLimit)
 	{
-	result = HIGH;	 
+	result = HIGHERROR;	 
+	}
+	else if(input > highWarningLimit)
+	{
+	  result = HIGHWARNING;
 	}
 	return result;
 }
 
 void printStatus( int parameter)
 {
-	printf(" %s is %s \n",batteryCond.parameter[parameter],batteryCond.status[parameter]);
+	printf(" %s : %s \n",batteryCond.parameter[parameter],batteryCond.status[parameter]);
 	
+}
+void StatusStore(int parameter,int lowerResult,int higherResult)
+{
+strcpy((batteryCond.parameter[parameter]), alertNotes[languageSelector].parameter[parameter]);
+strcpy(batteryCond.status[parameter], alertNotes[languageSelector].status[lowerResult + higherResult]);
 }
 
 bool limitCheck(float value, float *limit, int parameter)
 {
 	
-int lowerResult = checkLowerLimit(value , limit[0]);
+int lowerResult = checkLowerLimit(value , limit[0] , limit[1]);
 int higherResult = checkHigherLimit(value , limit[1]);
-strcpy((batteryCond.parameter[parameter]), batteryParameters[parameter]);
-strcpy(batteryCond.status[parameter], batteryStatus[lowerResult + higherResult]);
+StatusStore(parameter,lowerResult,higherResult);
 printStatus(parameter);
 return (lowerResult || higherResult);
 }
@@ -65,6 +81,7 @@ bool batteryIsOk(float temperature, float soc, float chargeRate, float *limitArr
 resultTemp = temperatureIsOk(temperature,&limitArray[0]);
 resultSoc = SOCIsOk(soc,&limitArray[2]);
 resultChargeRate = chargeRateIsOk(chargeRate,&limitArray[4]);
+
 return(resultTemp|| resultSoc||resultChargeRate);
 	
 }
